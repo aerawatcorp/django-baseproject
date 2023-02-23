959cd15747909b4badd74122724a61ebd2e746df
acpmasquerade <dhrubastergmail.com>
2023-02-23 23:40:39 +0545

    base project and local settings

--- baseproject/settings/base.py
+++ baseproject/settings/base.py
@@ -13,17 +13,18 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR is your Django project directory. The same directory where manage.py is located.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kp8zb#gt(m6^9gtcg8*$y*%e6n*@_0yt51f43*0qod&+!4egb1"
+SECRET_KEY = "" # Intentionally left blank for override in local or other environent specific settings
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False # Explicitly set to False for override in local or other environent specific settings
 
 ALLOWED_HOSTS = []
 
